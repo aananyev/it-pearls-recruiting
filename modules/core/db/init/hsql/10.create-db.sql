c2711c425c898124ac27eea865f34a6a2f7d30ef
malakay <a.ananjev@gmail.com>
2019-10-10 14:57:17 +0400

    решение фич

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -211,7 +211,7 @@ create table ITPEARLS_OPEN_POSITION (
     PROJECT_NAME_ID varchar(36) not null,
     COMPANY_NAME_ID varchar(36) not null,
     COMPANY_DEPARTAMENT_ID varchar(36),
-    NUMBER_POSITION integer not null,
+    NUMBER_POSITION integer,
     COMMENT_ longvarchar,
     --
     primary key (ID)
@@ -301,9 +301,10 @@ create table ITPEARLS_ITERACTION_LIST (
     PROJECT_ID varchar(36),
     COMPANY_DEPARTMENT_ID varchar(36) not null,
     ITERACTION_TYPE_ID varchar(36) not null,
-    RECRUTIER_ID varchar(36) not null,
     COMMUNICATION_METHOD varchar(80),
     COMMENT_ longvarchar,
+    RECRUTIER_ID varchar(36) not null,
+    RECRUTIER_NAME varchar(80),
     --
     primary key (ID)
 )^
@@ -364,24 +365,7 @@ create table ITPEARLS_JOB_HISTORY (
     primary key (ID)
 )^
 -- end ITPEARLS_JOB_HISTORY
--- begin ITPEARLS_CANDIDATES_SOCIAL_NETWORK
-create table ITPEARLS_CANDIDATES_SOCIAL_NETWORK (
-    ID varchar(36) not null,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    CANDIDATE_ID varchar(36) not null,
-    SOCIAL_NETWORK_ID varchar(36) not null,
-    SOCIAL_NETWORK_UPL varchar(80) not null,
-    --
-    primary key (ID)
-)^
--- end ITPEARLS_CANDIDATES_SOCIAL_NETWORK
+
 -- begin ITPEARLS_CANDIDATE_CV
 create table ITPEARLS_CANDIDATE_CV (
     ID varchar(36) not null,
