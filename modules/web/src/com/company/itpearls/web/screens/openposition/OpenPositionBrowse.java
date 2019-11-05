d3eb415f965694c886944fba4c7e7bb7294c5c1f
deneb3 <a.ananjev@gmail.com>
2019-11-06 00:06:49 +0400

    Открытые позиции

--- modules/web/src/com/company/itpearls/web/screens/openposition/OpenPositionBrowse.java
+++ modules/web/src/com/company/itpearls/web/screens/openposition/OpenPositionBrowse.java
@@ -1,11 +1,37 @@
 package com.company.itpearls.web.screens.openposition;
 
+import com.haulmont.cuba.gui.components.CheckBox;
+import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.OpenPosition;
+import com.haulmont.cuba.security.global.UserSession;
+
+import javax.inject.Inject;
 
 @UiController("itpearls_OpenPosition.browse")
 @UiDescriptor("open-position-browse.xml")
 @LookupComponent("openPositionsTable")
 @LoadDataBeforeShow
 public class OpenPositionBrowse extends StandardLookup<OpenPosition> {
+    @Inject
+    private CollectionLoader<OpenPosition> openPositionsDl;
+    @Inject
+    private CheckBox checkBoxOnlyOpenedPosition;
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        checkBoxOnlyOpenedPosition.setValue( false ); // только открытые позиции
+    }
+
+    @Subscribe("checkBoxOnlyOpenedPosition")
+    public void onCheckBoxOnlyOpenedPositionValueChange(HasValue.ValueChangeEvent<Boolean> event) {
+        if( checkBoxOnlyOpenedPosition.getValue() ) {
+            openPositionsDl.setParameter("openClosePos", false );
+        } else {
+            openPositionsDl.removeParameter("openClosePos");
+        }
+
+        openPositionsDl.load();
+    }
 }
