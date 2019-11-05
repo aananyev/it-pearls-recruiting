42dde7efc9c4b6de7730bca9691b6f6a6ee4148d
deneb3 <a.ananjev@gmail.com>
2019-11-05 21:12:12 +0400

    твои/мои взаимодействия

--- modules/web/src/com/company/itpearls/web/screens/iteractionlist/IteractionListBrowse.java
+++ modules/web/src/com/company/itpearls/web/screens/iteractionlist/IteractionListBrowse.java
@@ -1,9 +1,13 @@
 package com.company.itpearls.web.screens.iteractionlist;
 
+import com.haulmont.cuba.gui.components.CheckBox;
+import com.haulmont.cuba.gui.components.HasValue;
 import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.model.DataComponents;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.IteractionList;
+import com.haulmont.cuba.security.global.UserSession;
 
 import javax.inject.Inject;
 
@@ -12,4 +16,23 @@ import javax.inject.Inject;
 @LookupComponent("iteractionListsTable")
 @LoadDataBeforeShow
 public class IteractionListBrowse extends StandardLookup<IteractionList> {
+    @Inject
+    private UserSession userSession;
+    @Inject
+    private CollectionLoader<IteractionList> iteractionListsDl;
+    @Inject
+    private CheckBox checkBoxShowOnlyMy;
+
+    @Subscribe("checkBoxShowOnlyMy")
+    public void onCheckBoxShowOnlyMyValueChange(HasValue.ValueChangeEvent<Boolean> event) {
+        if(checkBoxShowOnlyMy.getValue()) {
+           iteractionListsDl.setParameter("userName", "%" + userSession.getUser().getLogin() + "%");
+        } else {
+            iteractionListsDl.removeParameter("userName");
+        }
+
+        iteractionListsDl.load();
+    }
+    
+    
 }
