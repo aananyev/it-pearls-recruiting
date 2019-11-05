06335ac7779e4bf4a0436c7be3c1e443078c0497
deneb3 <a.ananjev@gmail.com>
2019-11-05 17:26:06 +0400

    справочник типов взаимодействия

--- modules/web/src/com/company/itpearls/web/screens/iteraction/IteractionEdit.java
+++ modules/web/src/com/company/itpearls/web/screens/iteraction/IteractionEdit.java
@@ -1,11 +1,39 @@
 package com.company.itpearls.web.screens.iteraction;
 
+import com.haulmont.cuba.core.global.PersistenceHelper;
+import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.components.LookupPickerField;
+import com.haulmont.cuba.gui.components.TextField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.Iteraction;
+
+import javax.inject.Inject;
 
 @UiController("itpearls_Iteraction.edit")
 @UiDescriptor("iteraction-edit.xml")
 @EditedEntityContainer("iteractionDc")
 @LoadDataBeforeShow
 public class IteractionEdit extends StandardEditor<Iteraction> {
+    @Inject
+    private LookupPickerField<Iteraction> iteractionTreeField;
+    @Inject
+    private TextField<String> iterationNameField;
+
+    @Subscribe
+    public void onAfterShow(AfterShowEvent event) {
+       if(PersistenceHelper.isNew(getEditedEntity())) {
+           getEditedEntity().setMandatoryIteraction(false);
+       }
+    }
+
+    @Subscribe("iteractionCheckBoxMandatory")
+    public void onIteractionCheckBoxMandatoryValueChange(HasValue.ValueChangeEvent<Boolean> event) {
+        if (getEditedEntity().getMandatoryIteraction()) {
+            iteractionTreeField.setEditable(false);
+            iterationNameField.setEditable(false);
+        } else {
+            iteractionTreeField.setEditable(true);
+            iterationNameField.setEditable(true);
+        }
+    }
 }
