162785c4d635382927d9d3bef63a9ab259c65b64
deneb3 <a.ananjev@gmail.com>
2019-11-05 21:23:08 +0400

    твои/мои резюме

--- modules/web/src/com/company/itpearls/web/screens/candidatecv/CandidateCVBrowse.java
+++ modules/web/src/com/company/itpearls/web/screens/candidatecv/CandidateCVBrowse.java
@@ -2,7 +2,10 @@ package com.company.itpearls.web.screens.candidatecv;
 
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.UserSessionSource;
+import com.haulmont.cuba.gui.components.CheckBox;
+import com.haulmont.cuba.gui.components.HasValue;
 import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.CandidateCV;
 import com.haulmont.cuba.security.global.UserSession;
@@ -14,6 +17,25 @@ import javax.inject.Inject;
 @LookupComponent("candidateCVsTable")
 @LoadDataBeforeShow
 public class CandidateCVBrowse extends StandardLookup<CandidateCV> {
+    @Inject
+    private CollectionLoader<CandidateCV> candidateCVsDl;
+    @Inject
+    private CheckBox checkBoxSetOnlyMy;
+    @Inject
+    private UserSession userSession;
+
+    @Subscribe("checkBoxSetOnlyMy")
+    public void onCheckBoxSetOnlyMyValueChange(HasValue.ValueChangeEvent<Boolean> event) {
+       if(checkBoxSetOnlyMy.getValue()) {
+           candidateCVsDl.setParameter("userName", "%" + userSession.getUser().getLogin() + "%" );
+       } else {
+          candidateCVsDl.removeParameter( "userName");
+       }
+
+       candidateCVsDl.load();
+    }
+    
+    
 /*    private CollectionContainer<CandidateCV> candidateCVsSetupDc;
     @Inject
     private DataManager dataManager;
