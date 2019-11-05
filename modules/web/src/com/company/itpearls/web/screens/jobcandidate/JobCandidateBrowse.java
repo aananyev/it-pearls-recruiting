d97139ac56d9d9131b92c086eca89b3a287f7a7f
deneb3 <a.ananjev@gmail.com>
2019-11-05 20:55:05 +0400

    твои/мои кандидаты

--- modules/web/src/com/company/itpearls/web/screens/jobcandidate/JobCandidateBrowse.java
+++ modules/web/src/com/company/itpearls/web/screens/jobcandidate/JobCandidateBrowse.java
@@ -1,11 +1,36 @@
 package com.company.itpearls.web.screens.jobcandidate;
 
+import com.haulmont.cuba.gui.components.CheckBox;
+import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.JobCandidate;
+import com.haulmont.cuba.security.global.UserSession;
+
+import javax.inject.Inject;
 
 @UiController("itpearls_JobCandidate.browse")
 @UiDescriptor("job-candidate-browse.xml")
 @LookupComponent("jobCandidatesTable")
 @LoadDataBeforeShow
 public class JobCandidateBrowse extends StandardLookup<JobCandidate> {
+    @Inject
+    private CheckBox checkBoxShowOnlyMy;
+    @Inject
+    private CollectionLoader<JobCandidate> jobCandidatesDl;
+    @Inject
+    private UserSession userSession;
+
+    @Subscribe("checkBoxShowOnlyMy")
+    public void onCheckBoxShowOnlyMyValueChange(HasValue.ValueChangeEvent<Boolean> event) {
+        if(checkBoxShowOnlyMy.getValue()) {
+           jobCandidatesDl.setParameter("userName", "%" + userSession.getUser().getLogin() + "%");
+        }
+        else {
+            jobCandidatesDl.removeParameter("userName");
+        }
+
+        jobCandidatesDl.load();
+    }
 }
