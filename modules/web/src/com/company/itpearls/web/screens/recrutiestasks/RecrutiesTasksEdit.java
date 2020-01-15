72594b0ce3eb35ae187674cb1927cba8fb48d96d
deneb3 <a.ananjev@gmail.com>
2020-01-15 19:31:51 +0400

    корректировка задания рекрутеру

--- modules/web/src/com/company/itpearls/web/screens/recrutiestasks/RecrutiesTasksEdit.java
+++ modules/web/src/com/company/itpearls/web/screens/recrutiestasks/RecrutiesTasksEdit.java
@@ -1,11 +1,47 @@
 package com.company.itpearls.web.screens.recrutiestasks;
 
+import com.haulmont.cuba.core.global.UserSessionSource;
+import com.haulmont.cuba.gui.Dialogs;
+import com.haulmont.cuba.gui.components.DateField;
+import com.haulmont.cuba.gui.components.LookupPickerField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.RecrutiesTasks;
+import com.haulmont.cuba.security.entity.User;
+import com.haulmont.cuba.security.global.UserSession;
+
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.Date;
 
 @UiController("itpearls_RecrutiesTasks.edit")
 @UiDescriptor("recruties-tasks-edit.xml")
 @EditedEntityContainer("recrutiesTasksDc")
 @LoadDataBeforeShow
 public class RecrutiesTasksEdit extends StandardEditor<RecrutiesTasks> {
+    @Inject
+    private DateField<Date> startDateField;
+    @Inject
+    private UserSessionSource userSessionSource;
+    @Inject
+    private LookupPickerField<User> recrutiesTasksFieldUser;
+    @Inject
+    private UserSession userSession;
+    @Inject
+    private Dialogs dialogs;
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        String  role = "Researcher";
+
+        startDateField.setValue( new Date() );
+
+        // если роль - ресерчер, то автоматически вставить себя
+        Collection<String> s = userSessionSource.getUserSession().getRoles();
+        // установить поле рекрутера
+        if( s.contains(role) ) {
+            recrutiesTasksFieldUser.setValue(userSession.getUser());
+
+            recrutiesTasksFieldUser.setEnabled(false);
+        }
+    }
 }
