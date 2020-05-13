5b73abf0f7a6f6eee4703a227e0167a017b73cb8
Aleksey Ananyev <a.ananjev@gmail.com>
2020-05-13 23:51:50 +0400

    пробный виджет

---
+++ modules/web/src/com/company/itpearls/web/widgets/ItpearlsLogoWiget.java
@@ -0,0 +1,12 @@
+package com.company.itpearls.web.widgets;
+
+import com.haulmont.addon.dashboard.web.annotation.DashboardWidget;
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("itpearls_ItpearlsLogoWiget")
+@UiDescriptor("itpearls-logo-wiget.xml")
+@DashboardWidget(name="IT Perls Logo")
+public class ItpearlsLogoWiget extends ScreenFragment {
+}
