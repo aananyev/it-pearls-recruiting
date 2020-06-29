f72b9e6aad9b6de6c1f23c8ee5a96a799d70dd94
Aleksey Ananyev <a.ananjev@gmail.com>
2020-06-29 13:42:46 +0400

    недоделка виджета активных позиций

---
+++ modules/web/src/com/company/itpearls/web/widgets/reports/ActiveCandidates.java
@@ -0,0 +1,30 @@
+package com.company.itpearls.web.widgets.reports;
+
+import com.haulmont.addon.dashboard.web.annotation.DashboardWidget;
+import com.haulmont.cuba.gui.components.Label;
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.Subscribe;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+import javax.inject.Inject;
+
+@UiController("itpearls_ActiveCandidates")
+@UiDescriptor("active-candidates.xml")
+@DashboardWidget(name = "Active Candidates Widget")
+public class ActiveCandidates extends ScreenFragment {
+    @Inject
+    private Label<String> widgetTitle;
+
+    private String widgetTitleString = "Список активных кандидатов";
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+       setTitle();
+    }
+
+    private void setTitle() {
+        widgetTitle.setValue(widgetTitleString);
+    }
+
+}
