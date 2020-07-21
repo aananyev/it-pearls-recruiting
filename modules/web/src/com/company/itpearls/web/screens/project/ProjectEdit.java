1f2a1355673ee0b99b0246d3ec5fb9abd77ec0f2
Deneb <a.ananjev@gmail.com>
2020-07-21 22:14:22 +0400

    закрытие проекта - закрытие вакансий

--- modules/web/src/com/company/itpearls/web/screens/project/ProjectEdit.java
+++ modules/web/src/com/company/itpearls/web/screens/project/ProjectEdit.java
@@ -1,14 +1,20 @@
 package com.company.itpearls.web.screens.project;
 
 import com.company.itpearls.entity.CompanyDepartament;
+import com.company.itpearls.entity.OpenPosition;
 import com.company.itpearls.entity.Person;
+import com.haulmont.cuba.core.global.CommitContext;
+import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.PersistenceHelper;
+import com.haulmont.cuba.gui.Dialogs;
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.Project;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @UiController("itpearls_Project.edit")
 @UiDescriptor("project-edit.xml")
@@ -27,6 +33,55 @@ public class ProjectEdit extends StandardEditor<Project> {
     private LookupPickerField<CompanyDepartament> projectDepartmentField;
     @Inject
     private LookupPickerField<Person> projectOwnerField;
+    @Inject
+    private Dialogs dialogs;
+    @Inject
+    private DataManager dataManager;
+
+    List<OpenPosition> openPositions = new ArrayList<>();
+
+    @Subscribe("checkBoxProjectIsClosed")
+    public void onCheckBoxProjectIsClosedValueChange1(HasValue.ValueChangeEvent<Boolean> event) {
+        String opeedPositionList = "";
+
+        for(OpenPosition a : openPositions) {
+            opeedPositionList = a.getVacansyName() + "<br>" + opeedPositionList;
+        }
+
+        if(checkBoxProjectIsClosed.getValue()) {
+            dialogs.createOptionDialog()
+                    .withCaption("ВНИМАНИЕ")
+                    .withType(Dialogs.MessageType.WARNING)
+                    .withContentMode(ContentMode.HTML)
+                    .withMessage("<b>Закрыть вакансии на этом проекте?</b><br>Открыты позиции: <br><i>" +
+                            opeedPositionList + "</i>")
+                    .withActions(
+                            new DialogAction(DialogAction.Type.YES, Action.Status.PRIMARY).withHandler(e -> {
+                                closeAllVacansies();
+                            }),
+                            new DialogAction((DialogAction.Type.NO)
+                            ))
+                    .show();
+            
+            setEndDateProject();
+        }
+
+    }
+
+    private void setEndDateProject() {
+        Date date = new Date();
+
+        endProjectDateField.setValue(date);
+    }
+
+    private void closeAllVacansies() {
+        for (OpenPosition a : openPositions) {
+            a.setOpenClose(true);
+
+            CommitContext commitContext = new CommitContext(a);
+            dataManager.commit(commitContext);
+        }
+    }
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
@@ -35,6 +90,18 @@ public class ProjectEdit extends StandardEditor<Project> {
 
            startProjectDateField.setValue( date );
        }
+
+       getOpenedPosition();
+    }
+
+    private void getOpenedPosition() {
+        String positionsQuery = "select e from itpearls_OpenPosition e " +
+                "where e.projectName = :projectName and " +
+                "e.openClose = false";
+        openPositions = dataManager.load(OpenPosition.class)
+                .query(positionsQuery)
+                .parameter("projectName", getEditedEntity())
+                .list();
     }
 
     @Subscribe("checkBoxProjectIsClosed")
