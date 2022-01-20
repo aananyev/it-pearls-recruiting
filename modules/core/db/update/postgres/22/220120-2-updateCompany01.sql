09e926408562c6b5e2d569dc8e40f299666f5fe7
Deneb <a.ananjev@gmail.com>
2022-01-20 23:19:57 +0400

    Компании и наши юрлица

---
+++ modules/core/db/update/postgres/22/220120-2-updateCompany01.sql
@@ -0,0 +1,1 @@
+alter table ITPEARLS_COMPANY rename column our_company to our_company__u97399 ;
--- modules/web/src/com/company/itpearls/web/screens/company/CompanyBrowse.java
+++ modules/web/src/com/company/itpearls/web/screens/company/CompanyBrowse.java
@@ -1,7 +1,10 @@
 package com.company.itpearls.web.screens.company;
 
 import com.haulmont.cuba.gui.components.CheckBox;
+import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.icons.CubaIcon;
+import com.haulmont.cuba.gui.icons.Icons;
 import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.Company;
@@ -13,10 +16,13 @@ import javax.inject.Inject;
 @LookupComponent("companiesTable")
 @LoadDataBeforeShow
 public class CompanyBrowse extends StandardLookup<Company> {
+
     @Inject
     private CheckBox checkBoxOnlyOurClient;
     @Inject
     private CollectionLoader<Company> companiesDl;
+    @Inject
+    private CheckBox checkBoxOnlyLegalEntity;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
@@ -28,6 +34,21 @@ public class CompanyBrowse extends StandardLookup<Company> {
         filterOurClients();
     }
 
+    @Subscribe("checkBoxOnlyLegalEntity")
+    public void onCheckBoxOnlyLegalEntityValueChange(HasValue.ValueChangeEvent<Boolean> event) {
+        filterOurLegalEntity();
+    }
+
+    private void filterOurLegalEntity() {
+        if (checkBoxOnlyLegalEntity.getValue()) {
+            companiesDl.setParameter("setOurLegalEntity", true);
+        } else {
+            companiesDl.removeParameter("setOurLegalEntity");
+        }
+
+        companiesDl.load();
+    }
+
     private void filterOurClients() {
         if (checkBoxOnlyOurClient.getValue()) {
             companiesDl.setParameter("setOurClient", true);
@@ -36,5 +57,73 @@ public class CompanyBrowse extends StandardLookup<Company> {
         }
 
         companiesDl.load();
+    }
+
+    @Install(to = "companiesTable.ourCompanyIconColumn", subject = "columnGenerator")
+    private Icons.Icon companiesTableOurCompanyIconColumnColumnGenerator(DataGrid.ColumnGeneratorEvent<Company> event) {
+        String returnIcon = "";
+
+        if(event.getItem().getOurLegalEntity() != null) {
+            if (event.getItem().getOurLegalEntity()) {
+                returnIcon = "PLUS_CIRCLE";
+            } else {
+                returnIcon = "MINUS_CIRCLE";
+            }
+        } else {
+            returnIcon = "MINUS_CIRCLE";
+        }
+
+        return CubaIcon.valueOf(returnIcon);
+    }
+
+    @Install(to = "companiesTable.ourClientIconColumn", subject = "columnGenerator")
+    private Icons.Icon companiesTableOurClientIconColumnColumnGenerator(DataGrid.ColumnGeneratorEvent<Company> event) {
+        String returnIcon = "";
+
+        if( event.getItem().getOurClient() != null) {
+            if (event.getItem().getOurClient()) {
+                returnIcon = "PLUS_CIRCLE";
+            } else {
+                returnIcon = "MINUS_CIRCLE";
+            }
+        } else {
+            returnIcon = "MINUS_CIRCLE";
+        }
+
+        return CubaIcon.valueOf(returnIcon);
+    }
+
+    @Install(to = "companiesTable.ourCompanyIconColumn", subject = "styleProvider")
+    private String companiesTableOurCompanyIconColumnStyleProvider(Company company) {
+        String style = "";
+
+        if(company.getOurLegalEntity() != null) {
+            if (company.getOurLegalEntity()) {
+                style = "open-position-pic-center-large-green";
+            } else {
+                style = "open-position-pic-center-large-red";
+            }
+        } else {
+            style = "open-position-pic-center-large-red";
+        }
+
+        return style;
+    }
+
+    @Install(to = "companiesTable.ourClientIconColumn", subject = "styleProvider")
+    private String companiesTableOurClientIconColumnStyleProvider(Company company) {
+        String style = "";
+
+        if(company.getOurClient() != null) {
+            if (company.getOurClient()) {
+                style = "open-position-pic-center-large-green";
+            } else {
+                style = "open-position-pic-center-large-red";
+            }
+        } else {
+            style = "open-position-pic-center-large-red";
+        }
+
+        return style;
     }
 }
