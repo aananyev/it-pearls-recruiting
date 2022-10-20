5de9aab4887602f27cbc2864855067fc5ab792e1
Deneb <a.ananjev@gmail.com>
2022-10-20 11:52:27 +0400

    Исправление мелких неприятных ошибок

---
+++ modules/web/src/com/company/itpearls/web/StandartPriorityVacancy.java
@@ -0,0 +1,27 @@
+package com.company.itpearls.web;
+
+public class StandartPriorityVacancy {
+        public final static String DRAFT_STR = "Draft";
+        public final static Integer DRAFT_INT = -1;
+        public final static String DRAFT_ICON = "icons/traffic-lights_gray.png";
+
+        public final static String PAUSED_STR = "Paused";
+        public final static Integer PAUSED_INT = 0;
+        public final static String PAUSED_ICON = "icons/remove.png";
+
+        public final static String LOW_STR = "Low";
+        public final static Integer LOW_INT = 1;
+        public final static String LOW_ICON = "icons/traffic-lights_blue.png";
+
+        public final static String NORMAL_STR = "Normal";
+        public final static Integer NORMAL_INT = 2;
+        public final static String NORMAL_ICON = "icons/traffic-lights_green.png";
+
+        public final static String HIGH_STR = "High";
+        public final static Integer HIGH_INT = 3;
+        public final static String HIGH_ICON = "icons/traffic-lights_yellow.png";
+
+        public final static String CRITICAL_STR = "Critical";
+        public final static Integer CRITICAL_INT = 4;
+        public final static String CRITICAL_ICON = "icons/traffic-lights_red.png";
+}
