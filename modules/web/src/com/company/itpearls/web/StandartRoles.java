5b0b2195ada80f392ecbb01b3367ed8622fe4d4e
Deneb <a.ananjev@gmail.com>
2022-10-20 17:41:02 +0400

    Исправления ошибок

---
+++ modules/web/src/com/company/itpearls/web/StandartRoles.java
@@ -0,0 +1,9 @@
+package com.company.itpearls.web;
+
+public class StandartRoles {
+    public final static String RESEARCHER = "Researcher";
+    public final static String RECRUITER = "Recruiter";
+    public final static String MANAGER = "Manager";
+    public final static String ADMINISTRATOR = "Administrators";
+    public final static String OUSTAFF_NAMAGER = "Outstaff Manager";
+}
