1f637b3e3eb4371173f3351fd1bd699be9b36f09
Deneb <a.ananjev@gmail.com>
2022-10-25 11:08:45 +0400

    Добавлен screen fragment который парсит скилы кандидатов или резюме и размещает их в основных экранах

---
+++ modules/web/src/com/company/itpearls/web/screens/fragments/Skillsbar.java
@@ -0,0 +1,10 @@
+package com.company.itpearls.web.screens.fragments;
+
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("itpearls_Skillsbar")
+@UiDescriptor("skillsBar.xml")
+public class Skillsbar extends ScreenFragment {
+}
