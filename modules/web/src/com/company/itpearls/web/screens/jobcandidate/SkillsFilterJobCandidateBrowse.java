4da7ee5bb10e864e875c89f61347b7d4b640e916
Aleksey Ananyev <deneb@yandex.ru>
2022-12-23 22:50:59 +0400

    Изобрел поиск кандидатов по скиллам

--- modules/web/src/com/company/itpearls/web/screens/jobcandidate/SkillsFilterJobCandidateBrowse.java
+++ modules/web/src/com/company/itpearls/web/screens/jobcandidate/SkillsFilterJobCandidateBrowse.java
@@ -1,25 +1,26 @@
 package com.company.itpearls.web.screens.jobcandidate;
 
+import com.company.itpearls.entity.IteractionList;
 import com.company.itpearls.entity.SkillTree;
-import com.company.itpearls.web.SkillsFilterCandidateEvent;
 import com.company.itpearls.web.StandartPrioritySkills;
+import com.company.itpearls.web.screens.iteractionlist.IteractionListSimpleBrowse;
+import com.haulmont.cuba.core.entity.FileDescriptor;
 import com.haulmont.cuba.core.global.DataManager;
-import com.haulmont.cuba.core.global.Events;
+import com.haulmont.cuba.gui.Screens;
 import com.haulmont.cuba.gui.UiComponents;
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.data.value.ContainerValueSource;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.JobCandidate;
 import com.haulmont.cuba.gui.screen.LookupComponent;
 import com.haulmont.cuba.security.global.UserSession;
 import org.jsoup.Jsoup;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @UiController("itpearls_SkillsFilterJobCandidate.browse")
 @UiDescriptor("skills-filter-job-candidate-browse.xml")
@@ -27,13 +28,15 @@ import java.util.Map;
 @LoadDataBeforeShow
 public class SkillsFilterJobCandidateBrowse extends StandardLookup<JobCandidate> {
     private static final String QUERY_SKILL_TREE_GROUP
-            = "select e from itpearls_SkillTree e where e.skillTree is null order by e.skillName";
+            = "select e " +
+            "from itpearls_SkillTree e " +
+            "where e.skillTree is null and " +
+                "e in (select f.skillTree from itpearls_SkillTree f where f.skillTree = e) " +
+            "order by e.skillName";
     private static final String QUERY_SKILL_TREE_ITEM
             = "select e from itpearls_SkillTree e where e.skillTree = :skillGroup";
 
     private List<SkillTree> skillTreeGroup = new ArrayList<>();
-    private HashMap<SkillTree, GroupBoxLayout> skillGroupGroupBoxLayouts = new HashMap<>();
-    private HashMap<SkillTree, GroupBoxLayout> skillGroupFilterGroupBoxLayouts = new HashMap<>();
     private HashMap<LinkButton, LinkButton> skillsPairAllToFilter = new HashMap<>();
     private HashMap<LinkButton, LinkButton> skillsPairFilterToAll = new HashMap<>();
 
@@ -46,26 +49,90 @@ public class SkillsFilterJobCandidateBrowse extends StandardLookup<JobCandidate>
     @Inject
     private DataManager dataManager;
     @Inject
-    private Events events;
-    @Inject
     private UserSession userSession;
-
-    @Order(15)
-    @EventListener
-    protected void onSkillSelected(SkillsFilterCandidateEvent event) {
-        skillsPairAllToFilter.get((LinkButton) event.getSource()).setVisible(true);
-    }
+    @Inject
+    private DataGrid<JobCandidate> jobCandidatesTable;
+    @Inject
+    private Screens screens;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
         skillTreeGroup = getSkillTreeGroup();
 
-//        setSkillTreesGroupGroupBoxLayouts(skillGroupGroupBoxLayouts);
-//        setSkillTreesGroupGroupBoxLayouts(skillGroupFilterGroupBoxLayouts);
-
-//        addSkillsTreeGroupGroupBoxes(skillGroupGroupBoxLayouts, allSkillsScrollBox, true);
-//        addSkillsTreeGroupGroupBoxes(skillGroupFilterGroupBoxLayouts, filterSkillsScrollBox, false);
         addSkillTreeGroupBoxes();
+        candidateImageColumnRenderer();
+        candidateLastInteractionColumnRenderer();
+    }
+
+    private void candidateLastInteractionColumnRenderer() {
+        DataGrid.ClickableTextRenderer<JobCandidate> jobCandidatesTableLastIteractionRenderer =
+                jobCandidatesTable.createRenderer(DataGrid.ClickableTextRenderer.class);
+
+        jobCandidatesTableLastIteractionRenderer.setRendererClickListener(clickableTextRendererClickEvent -> {
+            IteractionListSimpleBrowse iteractionListSimpleBrowse = screens.create(IteractionListSimpleBrowse.class);
+            iteractionListSimpleBrowse.setSelectedCandidate(clickableTextRendererClickEvent.getItem());
+            screens.show(iteractionListSimpleBrowse);
+        });
+
+        jobCandidatesTable.getColumn("lastIteraction").setStyleProvider(e -> {
+            IteractionList iteractionList = getLastIteraction(e);
+            String retStr = "";
+
+            if (iteractionList != null) {
+                Calendar calendar = Calendar.getInstance();
+
+                if (iteractionList.getDateIteraction() != null) {
+                    calendar.setTime(iteractionList.getDateIteraction());
+                }
+
+                calendar.add(Calendar.MONTH, 1);
+
+                Calendar calendar1 = Calendar.getInstance();
+
+                if (calendar.after(calendar1)) {
+                    if (iteractionList.getRecrutier() != null) {
+                        if (!iteractionList.getRecrutier().equals(userSession.getUser())) {
+                            retStr = "button_table_red";
+                        } else {
+                            retStr = "button_table_yellow";
+                        }
+                    } else {
+                        retStr = "button_table_white";
+                    }
+                } else {
+                    retStr = "button_table_green";
+                }
+            } else {
+                retStr = "button_table_white";
+            }
+
+            return retStr;
+        });
+
+        jobCandidatesTable.getColumn("lastIteraction")
+                .setRenderer(jobCandidatesTable.createRenderer(DataGrid.HtmlRenderer.class));
+
+    }
+
+
+    private IteractionList getLastIteraction(JobCandidate jobCandidate) {
+        if (jobCandidate.getIteractionList() != null) {
+            IteractionList maxIteraction = null;
+
+            for (IteractionList iteractionList : jobCandidate.getIteractionList()) {
+                if (maxIteraction == null)
+                    maxIteraction = iteractionList;
+
+                if (iteractionList.getNumberIteraction() != null) {
+                    if (maxIteraction.getNumberIteraction().compareTo(iteractionList.getNumberIteraction()) < 0) {
+                        maxIteraction = iteractionList;
+                    }
+                }
+            }
+
+            return maxIteraction;
+        } else
+            return null;
     }
 
     private GroupBoxLayout setSkillGroupBox(SkillTree skillTree) {
@@ -123,25 +190,34 @@ public class SkillsFilterJobCandidateBrowse extends StandardLookup<JobCandidate>
         FlowBoxLayout flowBoxLayoutLeft = setFlowBoxLayout();
         FlowBoxLayout flowBoxLayoutRight = setFlowBoxLayout();
 
-        for (SkillTree st : skillTree) {
-            // левый
-            LinkButton labelLeft = setLinkButton(st);
-            LinkButton labelRight = setLinkButton(st);
-            labelRight.setVisible(false);
+        if (skillTree.size() != 0) {
+            for (SkillTree st : skillTree) {
+                // левый
+                LinkButton labelLeft = setLinkButton(st);
+                LinkButton labelRight = setLinkButton(st);
+                labelRight.setVisible(false);
 
-            labelLeft.addClickListener(e -> {
-                reverseVisible(e);
-            });
+                labelLeft.addClickListener(e -> {
+                    reverseVisible(e);
+                });
 
-            labelRight.addClickListener(e -> {
-                reverseVisibleFilter(e);
-            });
+                labelRight.addClickListener(e -> {
+                    reverseVisibleFilter(e);
+                });
 
-            skillsPairAllToFilter.put(labelLeft, labelRight);
-            skillsPairFilterToAll.put(labelRight, labelLeft);
+                skillsPairAllToFilter.put(labelLeft, labelRight);
+                skillsPairFilterToAll.put(labelRight, labelLeft);
 
-            flowBoxLayoutLeft.add(labelLeft);
-            flowBoxLayoutRight.add(labelRight);
+                flowBoxLayoutLeft.add(labelLeft);
+                flowBoxLayoutRight.add(labelRight);
+
+            }
+
+            groupBoxLayoutLeft.setVisible(true);
+            groupBoxLayoutRight.setVisible(false);
+        } else {
+            groupBoxLayoutLeft.setVisible(false);
+            groupBoxLayoutRight.setVisible(false);
         }
 
         groupBoxLayoutLeft.add(flowBoxLayoutLeft);
@@ -152,82 +228,28 @@ public class SkillsFilterJobCandidateBrowse extends StandardLookup<JobCandidate>
         e.getSource().setVisible(!e.getSource().isVisible());
         skillsPairFilterToAll.get(e.getSource()).setVisible(
                 !skillsPairFilterToAll.get(e.getSource()).isVisible());
-    }
 
-    private void setSkillTreesGroupGroupBoxLayouts(HashMap<SkillTree, GroupBoxLayout> skillBoxLayouts) {
-        for (SkillTree skillTree : skillTreeGroup) {
-            GroupBoxLayout groupBoxLayout = uiComponents.create(GroupBoxLayout.class);
+        skillsPairFilterToAll.get(e.getSource())
+                .getParent()
+                .getParent()
+                .setVisible(skillsPairFilterToAll
+                        .get(e.getSource())
+                        .isVisible());
 
-            groupBoxLayout.setStyleName("light");
-            groupBoxLayout.setCollapsable(true);
-            groupBoxLayout.setCaption(skillTree.getSkillName());
-            groupBoxLayout.setDescription(skillTree.getComment() != null
-                    ? Jsoup.parse(skillTree.getComment()).text() : "");
-            groupBoxLayout.setWidthFull();
+        if (!e.getSource().isVisible()) {
+            Boolean flag = false;
+            for (Component component : ((FlowBoxLayout) e.getSource().getParent()).getComponents()) {
+                if (component.isVisible()) {
+                    flag = true;
 
-            skillBoxLayouts.put(skillTree, groupBoxLayout);
-        }
-    }
-
-    private void addSkillsTreeGroupGroupBoxes(HashMap<SkillTree, GroupBoxLayout> skillBoxLayouts,
-                                              ScrollBoxLayout scrollBox,
-                                              Boolean visible) {
-        for (Map.Entry<SkillTree, GroupBoxLayout> groupBoxLayoutEntry : skillBoxLayouts.entrySet()) {
-            addSkillLabels(groupBoxLayoutEntry, scrollBox, visible);
-            scrollBox.add(groupBoxLayoutEntry.getValue());
-
-        }
-    }
-
-    private void addSkillLabels(Map.Entry<SkillTree, GroupBoxLayout> groupBoxLayoutEntry,
-                                ScrollBoxLayout scrollBoxLayout,
-                                Boolean visible) {
-        List<SkillTree> skillTree = dataManager.load(SkillTree.class)
-                .query(QUERY_SKILL_TREE_ITEM)
-                .parameter("skillGroup", groupBoxLayoutEntry.getKey())
-                .list();
-
-        FlowBoxLayout hBoxLayout = uiComponents.create(FlowBoxLayout.class);
-        hBoxLayout.setWidthAuto();
-        hBoxLayout.setSpacing(true);
-
-        for (SkillTree st : skillTree) {
-            LinkButton label = uiComponents.create(LinkButton.class);
-            label.setCaption(st.getSkillName());
-            label.setStyleName(getStyleForSkillPriority(st));
-            label.setVisible(visible);
-
-            label.addClickListener(e -> {
-                reverseVisible(e);
-            });
-
-            hBoxLayout.add(label);
-
-            if (visible) {
-                skillsPairAllToFilter.put(label, null);
-
-                for (Map.Entry<LinkButton, LinkButton> entry : skillsPairFilterToAll.entrySet()) {
-                    if (entry.getKey().getCaption().equals(label.getCaption())) {
-                        skillsPairFilterToAll.replace(entry.getKey(), label);
-                    }
-                }
-            } else {
-                skillsPairFilterToAll.put(label, null);
-
-                for (Map.Entry<LinkButton, LinkButton> entry : skillsPairAllToFilter.entrySet()) {
-                    if (entry.getKey().getCaption().equals(label.getCaption())) {
-                        skillsPairAllToFilter.replace(entry.getKey(), label);
-                    }
+                    break;
                 }
             }
-        }
 
-        groupBoxLayoutEntry.getValue().add(hBoxLayout);
-
-        if (skillTree.size() == 0) {
-            groupBoxLayoutEntry.getValue().setVisible(false);
-        } else {
-            groupBoxLayoutEntry.getValue().setVisible(true);
+            e.getSource()
+                    .getParent()
+                    .getParent()
+                    .setVisible(flag);
         }
     }
 
@@ -235,6 +257,57 @@ public class SkillsFilterJobCandidateBrowse extends StandardLookup<JobCandidate>
         e.getSource().setVisible(!e.getSource().isVisible());
         skillsPairAllToFilter.get(e.getSource()).setVisible(
                 !skillsPairAllToFilter.get(e.getSource()).isVisible());
+        skillsPairAllToFilter.get(e.getSource())
+                .getParent()
+                .getParent()
+                .setVisible(skillsPairAllToFilter
+                        .get(e.getSource())
+                        .isVisible());
+
+        if (!e.getSource().isVisible()) {
+            Boolean flag = false;
+            for (Component component : ((FlowBoxLayout) e.getSource().getParent()).getComponents()) {
+                if (component.isVisible()) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            e.getSource()
+                    .getParent()
+                    .getParent()
+                    .setVisible(flag);
+        }
+    }
+
+    private void candidateImageColumnRenderer() {
+        jobCandidatesTable.addGeneratedColumn("fileImageFace", entity -> {
+            HBoxLayout hBox = uiComponents.create(HBoxLayout.class);
+            Image image = uiComponents.create(Image.NAME);
+
+            if (entity.getItem().getFileImageFace() != null) {
+                try {
+                    image.setValueSource(new ContainerValueSource<JobCandidate, FileDescriptor>(entity.getContainer(),
+                            "fileImageFace"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                image.setWidth("20px");
+                image.setStyleName("circle-20px");
+            } else {
+                image.setStyleName("pic-center");
+            }
+
+            image.setScaleMode(Image.ScaleMode.CONTAIN);
+            image.setAlignment(Component.Alignment.MIDDLE_CENTER);
+
+            hBox.setWidthFull();
+            hBox.setHeightFull();
+            hBox.add(image);
+
+            return hBox;
+        });
     }
 
     private String getStyleForSkillPriority(SkillTree st) {
@@ -244,31 +317,24 @@ public class SkillsFilterJobCandidateBrowse extends StandardLookup<JobCandidate>
             if (st.getPrioritySkill() != null) {
                 switch (st.getPrioritySkill()) {
                     case -1:
-//                    retLabel.setValue(StandartPrioritySkills.NOT_USED_SKILLS_STR);
                         retStr = StandartPrioritySkills.NOT_USED_SKILLS_STYLE;
                         break;
                     case 0:
-//                    retLabel.setValue(StandartPrioritySkills.DEFAULT_STR);
                         retStr = StandartPrioritySkills.DEFAULT_STYLE;
                         break;
                     case 1:
-//                    retLabel.setValue(StandartPrioritySkills.SUBJECT_AREA_STR);
                         retStr = StandartPrioritySkills.SUBJECT_AREA_STYLE;
                         break;
                     case 2:
-//                    retLabel.setValue(StandartPrioritySkills.FRAMEWORKS_STR);
                         retStr = StandartPrioritySkills.FRAMEWORKS_STYLE;
                         break;
                     case 3:
-//                    retLabel.setValue(StandartPrioritySkills.METHODOLOGY_STR);
                         retStr = StandartPrioritySkills.METHODOLORY_STYLE;
                         break;
                     case 4:
-//                    retLabel.setValue(StandartPrioritySkills.PROGRAMMING_LANGUAGE_STR);
                         retStr = StandartPrioritySkills.PROGRAMMING_LANGUAGE_STYLE;
                         break;
                     default:
-//                    retLabel.setValue(StandartPrioritySkills.NOT_USED_SKILLS_STR);
                         retStr = StandartPrioritySkills.NOT_USED_SKILLS_STYLE;
                         break;
                 }
