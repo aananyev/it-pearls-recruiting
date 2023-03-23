a9d3183a916d2d63fb3194ebd39e036904b01222
Aleksey Ananyev <deneb@yandex.ru>
2023-03-23 16:36:01 +0400

    Разделил открытые вакансии на рекрутинг и аутстафинг

---
+++ modules/web/src/com/company/itpearls/web/screens/openpositionrecruiting/OpenPositionRecruitingBrowse.java
@@ -0,0 +1,10 @@
+package com.company.itpearls.web.screens.openpositionrecruiting;
+
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.company.itpearls.web.screens.openposition.OpenPositionBrowse;
+
+@UiController("itpearls_OpenPositionRecruiting.browse")
+@UiDescriptor("open-position-browse-recrutiting.xml")
+public class OpenPositionRecruitingBrowse extends OpenPositionBrowse {
+}
