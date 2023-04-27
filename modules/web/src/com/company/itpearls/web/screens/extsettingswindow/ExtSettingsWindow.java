369f04a580bde19fa9ec3ec4ff1074d43023b522
Aleksey Ananyev <deneb@yandex.ru>
2023-04-27 13:43:32 +0400

    ExtSettingsWindow - добавил аккаунты для доступа к северу SMTP

---
+++ modules/web/src/com/company/itpearls/web/screens/extsettingswindow/ExtSettingsWindow.java
@@ -0,0 +1,6 @@
+package com.company.itpearls.web.screens.extsettingswindow;
+
+import com.haulmont.cuba.web.app.ui.core.settings.SettingsWindow;
+
+public class ExtSettingsWindow extends SettingsWindow {
+}
