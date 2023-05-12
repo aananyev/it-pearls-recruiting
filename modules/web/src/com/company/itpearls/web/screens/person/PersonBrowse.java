d24e55c5bfda1ff5398cc1db5b62f2911de11c91
Aleksey Ananyev <deneb@yandex.ru>
2023-05-12 14:55:36 +0400

    Картинки и лого доработал

--- modules/web/src/com/company/itpearls/web/screens/person/PersonBrowse.java
+++ modules/web/src/com/company/itpearls/web/screens/person/PersonBrowse.java
@@ -1,11 +1,50 @@
 package com.company.itpearls.web.screens.person;
 
+import com.haulmont.cuba.gui.UiComponents;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.itpearls.entity.Person;
+import com.haulmont.cuba.gui.screen.LookupComponent;
+
+import javax.inject.Inject;
 
 @UiController("itpearls_Person.browse")
 @UiDescriptor("person-browse.xml")
 @LookupComponent("personsTable")
 @LoadDataBeforeShow
 public class PersonBrowse extends StandardLookup<Person> {
+    @Inject
+    private UiComponents uiComponents;
+
+    @Install(to = "personsTable.personPicColumn", subject = "columnGenerator")
+    private Component personsTablePersonPicColumnColumnGenerator(DataGrid.ColumnGeneratorEvent<Person> event) {
+        HBoxLayout retBox = uiComponents.create(HBoxLayout.class);
+        retBox.setWidthFull();
+        retBox.setHeightFull();
+
+        Image image = uiComponents.create(Image.class);
+        image.setDescriptionAsHtml(true);
+        image.setScaleMode(Image.ScaleMode.SCALE_DOWN);
+        image.setWidth("20px");
+        image.setHeight("20px");
+        image.setStyleName("circle-20px");
+        image.setAlignment(Component.Alignment.MIDDLE_CENTER);
+        image.setDescription("<h4>"
+                + event.getItem().getFirstName()
+                + " "
+                + event.getItem().getSecondName()
+                + "</h4>");
+
+        if (event.getItem().getFileImageFace() != null) {
+            image.setSource(FileDescriptorResource.class)
+                    .setFileDescriptor(event
+                            .getItem()
+                            .getFileImageFace());
+        } else {
+            image.setSource(ThemeResource.class).setPath("icons/no-programmer.jpeg");
+        }
+
+        retBox.add(image);
+        return retBox;
+    }
 }
