9d72d8cc2fdaf2ce285ed0fc0192cdba03f507b3
Aleksey Ananyev <deneb@yandex.ru>
2023-06-02 16:21:34 +0400

    Настройки почты в ExtSettingsWindow

--- modules/web/src/com/company/itpearls/web/screens/extsettingswindow/ExtSettingsWindow.java
+++ modules/web/src/com/company/itpearls/web/screens/extsettingswindow/ExtSettingsWindow.java
@@ -2,15 +2,14 @@ package com.company.itpearls.web.screens.extsettingswindow;
 
 import com.company.itpearls.entity.ExtUser;
 import com.company.itpearls.entity.UserSettings;
-import com.haulmont.cuba.core.global.CommitContext;
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.core.global.UserSessionSource;
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.config.MenuItem;
+import com.haulmont.cuba.gui.model.DataContext;
 import com.haulmont.cuba.web.app.ui.core.settings.SettingsWindow;
 import javax.inject.Inject;
-import javax.persistence.NoResultException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -21,23 +20,17 @@ public class ExtSettingsWindow extends SettingsWindow {
     @Inject
     private TextField<String> smtpServer;
     @Inject
-    private TextField<String> smtpPort;
-    @Inject
     private CheckBox smtpPasswordRequired;
     @Inject
     private TextField<String> smtpPassword;
     @Inject
     private TextField<String> imapServer;
     @Inject
-    private TextField<String> imapPort;
-    @Inject
     private CheckBox imapPasswordRequired;
     @Inject
     private TextField<String> imapPassword;
     @Inject
     private TextField<String> pop3Server;
-    @Inject
-    private TextField<String> pop3Port;
     @Inject
     private CheckBox pop3PasswordRequired;
     @Inject
@@ -53,70 +46,143 @@ public class ExtSettingsWindow extends SettingsWindow {
     private UserSettings userSettings;
     private final String QUERY_GET_USER_SETTINGS
             = "select e from itpearls_UserSettings e where e.user = :currentUser";
+    @Inject
+    private DataContext dataContext;
+    @Inject
+    private Image defaultPic;
+    @Inject
+    private TextField<Integer> smtpPort;
+    @Inject
+    private TextField<Integer> imapPort;
+    @Inject
+    private TextField<Integer> pop3Port;
 
     @Override
     public void init(Map<String, Object> params) {
         currentUser = (ExtUser) userSessionSource.getUserSession().getUser();
-        userSettings = getUserSettingsEntity(currentUser);
+        getUserSettingsEntity(currentUser);
+
         setEmailSettings();
+
+        if (userPic.getSource() == null) {
+            userPic.setVisible(false);
+            defaultPic.setVisible(true);
+        } else {
+            userPic.setVisible(true);
+            defaultPic.setVisible(false);
+        }
 
         super.init(params);
     }
 
-    private UserSettings getUserSettingsEntity(ExtUser currentUser) {
-        UserSettings currentUserSetting;
-
+    private void getUserSettingsEntity(ExtUser currentUser) {
         try {
-            currentUserSetting = dataManager.load(UserSettings.class)
+            userSettings = dataManager.load(UserSettings.class)
                     .query(QUERY_GET_USER_SETTINGS)
                     .parameter("currentUser", currentUser)
                     .view("userSettings-view")
                     .one();
         } catch (IllegalStateException e) {
-            currentUserSetting = createNewUserSetting();
-            currentUserSetting.setUser((ExtUser) userSession.getUser());
-            dataManager.commit(currentUserSetting);
-        }
+            userSettings = createNewUserSetting();
+            setEmailSettings();
 
-        return currentUserSetting;
+        }
     }
 
     private void setEmailSettings() {
-        if (smtpServer.getValue() == null )
+        userSettings.setUser((ExtUser) userSession.getUser());
+
+        if (userSettings.getSmtpServer() != null) {
+            smtpServer.setValue(userSettings.getSmtpServer());
+        }
+
+        if (smtpServer.getValue() == null)
             smtpServer.setValue(currentUser.getSmtpServer());
 
-        if (smtpPort.getValue() == null)
+        if (userSettings.getSmtpPort() != null) {
+            smtpPort.setValue(userSettings.getSmtpPort());
+        }
+
+        if (smtpPort.getValue() == null) {
             smtpPort.setValue(currentUser.getSmtpPort() != null ?
-                    currentUser.getSmtpPort().toString() : null);
+                    currentUser.getSmtpPort() : 0);
+        }
+
+        if (userSettings.getSmtpPasswordRequired() != null) {
+            smtpPasswordRequired.setValue(userSettings.getSmtpPasswordRequired());
+        }
 
         if (smtpPasswordRequired.getValue() == null)
             smtpPasswordRequired.setValue(currentUser.getSmtpPasswordRequired());
 
         if (smtpPassword.getValue() == null)
-            smtpPassword.setValue(currentUser.getSmtpPassword());
+            smtpPassword.setValue(userSettings.getSmtpPassword());
+
+
+        if (userSettings.getImapServer() != null) {
+            imapServer.setValue(userSettings.getImapServer());
+        }
 
         if (imapServer.getValue() == null )
             imapServer.setValue(currentUser.getImapServer());
 
-        if (imapPort.getValue() == null)
+        if (userSettings.getImapPort() != null) {
+            imapPort.setValue(userSettings.getImapPort());
+        }
+
+        if (imapPort.getValue() == null) {
+            imapPort.setValue(currentUser.getImapPort());
+        }
+
+        if (imapPort.getValue() == null) {
             imapPort.setValue(currentUser.getImapPort() != null ?
-                    currentUser.getImapPort().toString() : null);
+                    currentUser.getImapPort() : 0);
+        }
+
+        if (userSettings.getImapPasswordRequired() != null) {
+            imapPasswordRequired.setValue(userSettings.getImapPasswordRequired());
+        }
 
         if (imapPasswordRequired.getValue() == null)
             imapPasswordRequired.setValue(currentUser.getImapPasswordRequired());
 
+
+        if (imapPassword.getValue() == null)
+            imapPassword.setValue(userSettings.getImapPassword());
+
         if (imapPassword.getValue() == null)
             imapPassword.setValue(currentUser.getImapPassword());
 
-        if (pop3Server.getValue() == null )
+
+        if (userSettings.getPop3Server() != null) {
+            pop3Server.setValue(userSettings.getPop3Server());
+        }
+
+        if (pop3Server.getValue() == null)
             pop3Server.setValue(currentUser.getPop3Server());
 
-        if (pop3Port.getValue() == null)
-            pop3Port.setValue(currentUser.getImapPort() != null ?
-                    currentUser.getPop3Port().toString() : null);
+        if (userSettings.getPop3Port() != null) {
+            pop3Port.setValue(userSettings.getPop3Port());
+        }
+
+        if (pop3Port.getValue() == null) {
+            pop3Port.setValue(currentUser.getPop3Port());
+        }
+
+        if (pop3Port.getValue() == null) {
+            pop3Port.setValue(currentUser.getPop3Port() != null ?
+                    currentUser.getPop3Port() : 0);
+        }
+
+        if (userSettings.getPop3PasswordRequired() != null) {
+            pop3PasswordRequired.setValue(userSettings.getPop3PasswordRequired());
+        }
 
         if (pop3PasswordRequired.getValue() == null)
             pop3PasswordRequired.setValue(currentUser.getPop3PasswordRequired());
+
+        if (pop3Password.getValue() == null)
+            pop3Password.setValue(userSettings.getPop3Password());
 
         if (pop3Password.getValue() == null)
             pop3Password.setValue(currentUser.getPop3Password());
@@ -144,25 +210,25 @@ public class ExtSettingsWindow extends SettingsWindow {
         return super.collectPermittedScreens(menuItems);
     }
 
-    UserSettings createNewUserSetting() {
+    private UserSettings createNewUserSetting() {
         UserSettings userSettingsNew = metadata.create(UserSettings.class);
 
         userSettingsNew.setUser((ExtUser) userSession.getUser());
         userSettingsNew.setSmtpServer(smtpServer.getValue());
         userSettingsNew.setSmtpPort(smtpPort.getValue() != null ?
-                Integer.parseInt(smtpPort.getValue()) : 0);
+                smtpPort.getValue() : 0);
         userSettingsNew.setSmtpPasswordRequired(smtpPasswordRequired.getValue());
         userSettingsNew.setSmtpPassword(smtpPassword.getValue());
 
         userSettingsNew.setPop3Server(pop3Server.getValue());
         userSettingsNew.setPop3Port(pop3Port.getValue() != null ?
-                Integer.parseInt(pop3Port.getValue()) : 0);
+                pop3Port.getValue() : 0);
         userSettingsNew.setPop3PasswordRequired(pop3PasswordRequired.getValue());
         userSettingsNew.setPop3Password(pop3Password.getValue());
 
         userSettingsNew.setImapServer(imapServer.getValue());
         userSettingsNew.setImapPort(imapPort.getValue() != null ?
-                Integer.parseInt(imapPort.getValue()) : 0);
+                imapPort.getValue() : 0);
         userSettingsNew.setImapPasswordRequired(imapPasswordRequired.getValue());
         userSettingsNew.setImapPassword(imapPassword.getValue());
 
@@ -172,9 +238,28 @@ public class ExtSettingsWindow extends SettingsWindow {
     @Override
     protected void commit() {
 
-        // CommitContext commitContext = new CommitContext(userSettingsNew, userSettings);
-        // dataManager.commit(commitContext);
+        userSettings.setSmtpServer(smtpServer.getValue());
+        userSettings.setSmtpPassword(smtpPassword.getValue());
+        userSettings.setSmtpPasswordRequired(smtpPasswordRequired.getValue());
 
+        userSettings.setSmtpPort(smtpPort.getValue() != null ?
+                smtpPort.getValue() : 0);
+
+        userSettings.setImapServer(imapServer.getValue());
+        userSettings.setImapPassword(imapPassword.getValue());
+        userSettings.setImapPasswordRequired(imapPasswordRequired.getValue());
+
+        userSettings.setImapPort(imapPort.getValue() != null ?
+                imapPort.getValue() : 0);
+
+        userSettings.setPop3Server(pop3Server.getValue());
+        userSettings.setPop3Password(pop3Password.getValue());
+        userSettings.setPop3PasswordRequired(pop3PasswordRequired.getValue());
+
+        userSettings.setPop3Port(pop3Port.getValue() != null ?
+                pop3Port.getValue() : 0);
+
+        dataManager.commit(userSettings);
         super.commit();
     }
 
