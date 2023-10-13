f3fed7f67c5bb01601343e81cd936ad98a40382c
Aleksey Ananyev <deneb@yandex.ru>
2023-10-13 13:35:25 +0400

    HunttechLogoWidget

---
+++ modules/web/src/com/company/itpearls/web/widgets/others/HunttechLogoWidget.java
@@ -0,0 +1,12 @@
+package com.company.itpearls.web.widgets.others;
+
+import com.haulmont.addon.dashboard.web.annotation.DashboardWidget;
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("itpearls_HunttechLogoWidget")
+@UiDescriptor("hunttech-logo-widget.xml")
+@DashboardWidget(name="HuntTech Logo")
+public class HunttechLogoWidget extends ScreenFragment {
+}
