fbe9711181a27d0cd8296efc4ebf64e415ed539e
Aleksey Ananyev <deneb@yandex.ru>
2024-06-19 10:35:32 +0400

    IteractionListPartners формы редактирования и просмотра

--- modules/web/src/com/company/itpearls/web/screens/iteractionlist/iteractionlistpartners/IteractionListPartnersBrowse.java
+++ modules/web/src/com/company/itpearls/web/screens/iteractionlist/iteractionlistpartners/IteractionListPartnersBrowse.java
@@ -2,6 +2,7 @@ package com.company.itpearls.web.screens.iteractionlist.iteractionlistpartners;
 
 import com.company.itpearls.core.PartnerPersonService;
 import com.company.itpearls.entity.IteractionListPartners;
+import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.Subscribe;
 import com.haulmont.cuba.gui.screen.UiController;
@@ -17,10 +18,17 @@ public class IteractionListPartnersBrowse extends IteractionListBrowse {
     private CollectionLoader<IteractionListPartners> iteractionListsDl;
     @Inject
     private PartnerPersonService partnerPersonService;
+    @Inject
+    private Button buttonExcel;
+
+    @Subscribe
+    public void onBeforeShow1(BeforeShowEvent event) {
+    }
 
     @Subscribe
     public void onAfterShow(AfterShowEvent event) {
         setIteractionListsDc();
+        buttonExcel.setVisible(false);
     }
 
     private void setIteractionListsDc() {
