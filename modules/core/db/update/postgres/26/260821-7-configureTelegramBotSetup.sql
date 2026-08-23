8d562be80591857335e027313d29fb698187009c
Aleksey Ananyev <deneb@yandex.ru>
2026-08-23 10:57:21 +0400

    Удалены чужие миграции 260821-*, 260822-* (не в master)

--- modules/core/db/update/postgres/26/260821-7-configureTelegramBotSetup.sql
+++
@@ -1,6 +0,0 @@
--- HRM HuntTech: Активация Telegram бота для активной конфигурации при наличии токена
-UPDATE HUNTTECH_APPLICATION_SETUP
-   SET TELEGRAM_BOT_START = true
- WHERE ACTIVE_SETUP = true
-   AND TELEGRAM_BOT_START IS NULL
-   AND NULLIF(TRIM(TELEGRAM_TOKEN), '') IS NOT NULL;
