9ab3cc08b93e02b5340c8a24c344c2d42254f8ed
Aleksey Ananyev <deneb@yandex.ru>
2026-08-23 21:07:05 +0400

    feat(geo): полный локальный справочник 85 субъектов РФ (ISO 3166-2:RU) с гербами Wikimedia для массовой загрузки регионов

--- modules/core/src/com/company/hunttech/service/GeoBulkLoaderServiceBean.java
+++ modules/core/src/com/company/hunttech/service/GeoBulkLoaderServiceBean.java
@@ -535,25 +535,131 @@ public class GeoBulkLoaderServiceBean implements GeoBulkLoaderService {
         }
     }
 
-    private void loadRegionsFromLocalCatalog(Country russia, LoadAccumulator acc) {
-        log.info("Загрузка регионов из локального справочника...");
-        // Вызываем enrichRegion для популярных регионов
-        String[] regions = {
-                "Москва", "Санкт-Петербург", "Московская область", "Республика Татарстан",
-                "Свердловская область", "Новосибирская область", "Краснодарский край",
-                "Ростовская область", "Башкортостан", "Челябинская область", "Нижегородская область"
-        };
+    /** Субъект РФ: ISO-код, русское название, тип, адм. центр, часовой пояс, URL герба (Wikimedia). */
+    private static class RegionLocalEntry {
+        final String iso;
+        final String nameRu;
+        final String type;
+        final String capital;
+        final String timeZone;
+        final String emblemUrl;
 
-        for (String regionName : regions) {
+        RegionLocalEntry(String iso, String nameRu, String type, String capital, String timeZone, String emblemUrl) {
+            this.iso = iso;
+            this.nameRu = nameRu;
+            this.type = type;
+            this.capital = capital;
+            this.timeZone = timeZone;
+            this.emblemUrl = emblemUrl;
+        }
+    }
+
+    /** Полный справочник субъектов РФ (ISO 3166-2:RU, 85 регионов) для массовой миграции.
+     *  Гербы — официальные файлы Wikimedia Commons (SVG). */
+    private static final List<RegionLocalEntry> RU_REGIONS_LOCAL = Arrays.asList(
+            new RegionLocalEntry("RU-AD", "Республика Адыгея", "Республика", "Майкоп", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/3/38/Coat_of_arms_of_Adygea.svg"),
+            new RegionLocalEntry("RU-AL", "Республика Алтай", "Республика", "Горно-Алтайск", "UTC+7", "https://upload.wikimedia.org/wikipedia/commons/1/1d/Coat_of_arms_of_Altai_Republic.svg"),
+            new RegionLocalEntry("RU-ALT", "Алтайский край", "Край", "Барнаул", "UTC+7", "https://upload.wikimedia.org/wikipedia/commons/3/3b/Coat_of_arms_of_Altai_Krai.svg"),
+            new RegionLocalEntry("RU-AMU", "Амурская область", "Область", "Благовещенск", "UTC+9", "https://upload.wikimedia.org/wikipedia/commons/9/94/Coat_of_arms_of_Amur_Oblast.svg"),
+            new RegionLocalEntry("RU-ARK", "Архангельская область", "Область", "Архангельск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/8/8b/Coat_of_Arms_of_Arkhangelsk_Oblast.svg"),
+            new RegionLocalEntry("RU-AST", "Астраханская область", "Область", "Астрахань", "UTC+4", "https://upload.wikimedia.org/wikipedia/commons/c/c3/Coat_of_arms_of_Astrakhan_Oblast.svg"),
+            new RegionLocalEntry("RU-BA", "Республика Башкортостан", "Республика", "Уфа", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/6/63/Coat_of_arms_of_Bashkortostan.svg"),
+            new RegionLocalEntry("RU-BEL", "Белгородская область", "Область", "Белгород", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/c/c0/Coat_of_arms_of_Belgorod_Oblast.svg"),
+            new RegionLocalEntry("RU-BRY", "Брянская область", "Область", "Брянск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/4/47/Coat_of_arms_of_Bryansk_Oblast.svg"),
+            new RegionLocalEntry("RU-BU", "Республика Бурятия", "Республика", "Улан-Удэ", "UTC+8", "https://upload.wikimedia.org/wikipedia/commons/c/c9/Coat_of_arms_of_Buryatia.svg"),
+            new RegionLocalEntry("RU-CE", "Чеченская Республика", "Республика", "Грозный", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/4/4b/Coat_of_arms_of_Chechnya.svg"),
+            new RegionLocalEntry("RU-CHE", "Челябинская область", "Область", "Челябинск", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/d/dd/Coat_of_arms_of_Chelyabinsk_Oblast.svg"),
+            new RegionLocalEntry("RU-CHU", "Чукотский автономный округ", "Автономный округ", "Анадырь", "UTC+12", "https://upload.wikimedia.org/wikipedia/commons/3/3e/Coat_of_arms_of_Chukotka.svg"),
+            new RegionLocalEntry("RU-CU", "Чувашская Республика", "Республика", "Чебоксары", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/c/c5/Coat_of_arms_of_Chuvashia.svg"),
+            new RegionLocalEntry("RU-DA", "Республика Дагестан", "Республика", "Махачкала", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/2/22/Coat_of_arms_of_Dagestan.svg"),
+            new RegionLocalEntry("RU-IN", "Республика Ингушетия", "Республика", "Магас", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/c/c6/Coat_of_arms_of_Ingushetia.svg"),
+            new RegionLocalEntry("RU-IRK", "Иркутская область", "Область", "Иркутск", "UTC+8", "https://upload.wikimedia.org/wikipedia/commons/5/5f/Coat_of_arms_of_Irkutsk_Oblast.svg"),
+            new RegionLocalEntry("RU-IVA", "Ивановская область", "Область", "Иваново", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/9/9e/Coat_of_arms_of_Ivanovo_Oblast.svg"),
+            new RegionLocalEntry("RU-KAM", "Камчатский край", "Край", "Петропавловск-Камчатский", "UTC+12", "https://upload.wikimedia.org/wikipedia/commons/2/24/Coat_of_arms_of_Kamchatka_Krai.svg"),
+            new RegionLocalEntry("RU-KB", "Кабардино-Балкарская Республика", "Республика", "Нальчик", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/5/51/Coat_of_arms_of_Kabardino-Balkaria.svg"),
+            new RegionLocalEntry("RU-KC", "Карачаево-Черкесская Республика", "Республика", "Черкесск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/e/e0/Coat_of_arms_of_Karachay-Cherkessia.svg"),
+            new RegionLocalEntry("RU-KDA", "Краснодарский край", "Край", "Краснодар", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/8/86/Coat_of_arms_of_Krasnodar_Krai.svg"),
+            new RegionLocalEntry("RU-KEM", "Кемеровская область", "Область", "Кемерово", "UTC+7", "https://upload.wikimedia.org/wikipedia/commons/1/1a/Coat_of_arms_of_Kemerovo_Oblast.svg"),
+            new RegionLocalEntry("RU-KGD", "Калининградская область", "Область", "Калининград", "UTC+2", "https://upload.wikimedia.org/wikipedia/commons/9/9f/Coat_of_arms_of_Kaliningrad_Oblast.svg"),
+            new RegionLocalEntry("RU-KGN", "Курганская область", "Область", "Курган", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/b/bd/Coat_of_arms_of_Kurgan_Oblast.svg"),
+            new RegionLocalEntry("RU-KHA", "Хабаровский край", "Край", "Хабаровск", "UTC+10", "https://upload.wikimedia.org/wikipedia/commons/f/f1/Coat_of_arms_of_Khabarovsk_Krai.svg"),
+            new RegionLocalEntry("RU-KHM", "Ханты-Мансийский автономный округ — Югра", "Автономный округ", "Ханты-Мансийск", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/6/6a/Coat_of_arms_of_Khanty-Mansiysk.svg"),
+            new RegionLocalEntry("RU-KIR", "Кировская область", "Область", "Киров", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/1/1f/Coat_of_arms_of_Kirov_Oblast.svg"),
+            new RegionLocalEntry("RU-KK", "Республика Хакасия", "Республика", "Абакан", "UTC+7", "https://upload.wikimedia.org/wikipedia/commons/c/c7/Coat_of_arms_of_Khakassia.svg"),
+            new RegionLocalEntry("RU-KL", "Республика Калмыкия", "Республика", "Элиста", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/9/99/Coat_of_arms_of_Kalmykia.svg"),
+            new RegionLocalEntry("RU-KLU", "Калужская область", "Область", "Калуга", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/3/33/Coat_of_arms_of_Kaluga_Oblast.svg"),
+            new RegionLocalEntry("RU-KO", "Республика Коми", "Республика", "Сыктывкар", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/5/5c/Coat_of_arms_of_the_Komi_Republic.svg"),
+            new RegionLocalEntry("RU-KOS", "Костромская область", "Область", "Кострома", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/9/98/Coat_of_arms_of_Kostroma_Oblast.svg"),
+            new RegionLocalEntry("RU-KR", "Республика Карелия", "Республика", "Петрозаводск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/e/ea/Coat_of_arms_of_Republic_of_Karelia.svg"),
+            new RegionLocalEntry("RU-KRS", "Курская область", "Область", "Курск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/2/2c/Coat_of_arms_of_Kursk_Oblast.svg"),
+            new RegionLocalEntry("RU-KYA", "Красноярский край", "Край", "Красноярск", "UTC+7", "https://upload.wikimedia.org/wikipedia/commons/2/2e/Coat_of_arms_of_Krasnoyarsk_Krai.svg"),
+            new RegionLocalEntry("RU-LEN", "Ленинградская область", "Область", "Санкт-Петербург", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/c/c1/Coat_of_arms_of_Leningrad_Oblast.svg"),
+            new RegionLocalEntry("RU-LIP", "Липецкая область", "Область", "Липецк", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/3/3c/Coat_of_arms_of_Lipetsk_Oblast.svg"),
+            new RegionLocalEntry("RU-MAG", "Магаданская область", "Область", "Магадан", "UTC+11", "https://upload.wikimedia.org/wikipedia/commons/1/11/Coat_of_arms_of_Magadan_Oblast.svg"),
+            new RegionLocalEntry("RU-ME", "Республика Марий Эл", "Республика", "Йошкар-Ола", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/2/2a/Coat_of_arms_of_Mari_El.svg"),
+            new RegionLocalEntry("RU-MO", "Республика Мордовия", "Республика", "Саранск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/3/3c/Coat_of_arms_of_Mordovia.svg"),
+            new RegionLocalEntry("RU-MOS", "Московская область", "Область", "Красногорск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/5/53/Coat_of_arms_of_Moscow_oblast.svg"),
+            new RegionLocalEntry("RU-MOW", "Москва", "Город федерального значения", "Москва", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/c/ca/Coat_of_Arms_of_Moscow.svg"),
+            new RegionLocalEntry("RU-MUR", "Мурманская область", "Область", "Мурманск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/9/95/Coat_of_arms_of_Murmansk_Oblast.svg"),
+            new RegionLocalEntry("RU-NEN", "Ненецкий автономный округ", "Автономный округ", "Нарьян-Мар", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/6/67/Coat_of_arms_of_Nenets_Autonomous_Okrug.svg"),
+            new RegionLocalEntry("RU-NGR", "Новгородская область", "Область", "Великий Новгород", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/e/e0/Coat_of_arms_of_Novgorod_Oblast.svg"),
+            new RegionLocalEntry("RU-NIZ", "Нижегородская область", "Область", "Нижний Новгород", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/9/9e/Coat_of_arms_of_Nizhny_Novgorod_Oblast.svg"),
+            new RegionLocalEntry("RU-NVS", "Новосибирская область", "Область", "Новосибирск", "UTC+7", "https://upload.wikimedia.org/wikipedia/commons/4/4b/Coat_of_arms_of_Novosibirsk_Oblast.svg"),
+            new RegionLocalEntry("RU-OMS", "Омская область", "Область", "Омск", "UTC+6", "https://upload.wikimedia.org/wikipedia/commons/8/8d/Coat_of_arms_of_Omsk_Oblast.svg"),
+            new RegionLocalEntry("RU-ORE", "Оренбургская область", "Область", "Оренбург", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/2/25/Coat_of_arms_of_Orenburg_Oblast.svg"),
+            new RegionLocalEntry("RU-ORL", "Орловская область", "Область", "Орёл", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/c/c6/Coat_of_arms_of_Oryol_Oblast.svg"),
+            new RegionLocalEntry("RU-PER", "Пермский край", "Край", "Пермь", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/2/28/Coat_of_arms_of_Perm_Krai.svg"),
+            new RegionLocalEntry("RU-PNZ", "Пензенская область", "Область", "Пенза", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/8/85/Coat_of_arms_of_Penza_Oblast.svg"),
+            new RegionLocalEntry("RU-PRI", "Приморский край", "Край", "Владивосток", "UTC+10", "https://upload.wikimedia.org/wikipedia/commons/c/c3/Coat_of_arms_of_Primorsky_Krai.svg"),
+            new RegionLocalEntry("RU-PSK", "Псковская область", "Область", "Псков", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/f/f0/Coat_of_arms_of_Pskov_Oblast.svg"),
+            new RegionLocalEntry("RU-ROS", "Ростовская область", "Область", "Ростов-на-Дону", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/1/14/Coat_of_arms_of_Rostov_Oblast.svg"),
+            new RegionLocalEntry("RU-RYA", "Рязанская область", "Область", "Рязань", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/8/8d/Coat_of_arms_of_Ryazan_Oblast.svg"),
+            new RegionLocalEntry("RU-SA", "Республика Саха (Якутия)", "Республика", "Якутск", "UTC+9", "https://upload.wikimedia.org/wikipedia/commons/c/c1/Coat_of_arms_of_the_Sakha_Republic.svg"),
+            new RegionLocalEntry("RU-SAK", "Сахалинская область", "Область", "Южно-Сахалинск", "UTC+11", "https://upload.wikimedia.org/wikipedia/commons/c/c5/Coat_of_arms_of_Sakhalin_Oblast.svg"),
+            new RegionLocalEntry("RU-SAM", "Самарская область", "Область", "Самара", "UTC+4", "https://upload.wikimedia.org/wikipedia/commons/4/4a/Coat_of_arms_of_Samara_Oblast.svg"),
+            new RegionLocalEntry("RU-SAR", "Саратовская область", "Область", "Саратов", "UTC+4", "https://upload.wikimedia.org/wikipedia/commons/9/95/Coat_of_arms_of_Saratov_Oblast.svg"),
+            new RegionLocalEntry("RU-SE", "Республика Северная Осетия — Алания", "Республика", "Владикавказ", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/6/60/Coat_of_arms_of_North_Ossetia.svg"),
+            new RegionLocalEntry("RU-SMO", "Смоленская область", "Область", "Смоленск", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/7/72/Coat_of_arms_of_Smolensk_Oblast.svg"),
+            new RegionLocalEntry("RU-SPE", "Санкт-Петербург", "Город федерального значения", "Санкт-Петербург", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/5/53/Coat_of_Arms_of_Saint_Petersburg_%282003%29.svg"),
+            new RegionLocalEntry("RU-STA", "Ставропольский край", "Край", "Ставрополь", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/9/98/Coat_of_arms_of_Stavropol_Krai.svg"),
+            new RegionLocalEntry("RU-SVE", "Свердловская область", "Область", "Екатеринбург", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/4/4f/Coat_of_arms_of_Sverdlovsk_Oblast.svg"),
+            new RegionLocalEntry("RU-TA", "Республика Татарстан", "Республика", "Казань", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/e/eb/Coat_of_arms_of_Tatarstan.svg"),
+            new RegionLocalEntry("RU-TAM", "Тамбовская область", "Область", "Тамбов", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/9/9f/Coat_of_arms_of_Tambov_Oblast.svg"),
+            new RegionLocalEntry("RU-TOM", "Томская область", "Область", "Томск", "UTC+7", "https://upload.wikimedia.org/wikipedia/commons/c/c6/Coat_of_arms_of_Tomsk_Oblast.svg"),
+            new RegionLocalEntry("RU-TUL", "Тульская область", "Область", "Тула", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/3/3f/Coat_of_arms_of_Tula_Oblast.svg"),
+            new RegionLocalEntry("RU-TVE", "Тверская область", "Область", "Тверь", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/b/b4/Coat_of_arms_of_Tver_Oblast.svg"),
+            new RegionLocalEntry("RU-TY", "Республика Тыва", "Республика", "Кызыл", "UTC+7", "https://upload.wikimedia.org/wikipedia/commons/9/9d/Coat_of_arms_of_Tuva.svg"),
+            new RegionLocalEntry("RU-TYU", "Тюменская область", "Область", "Тюмень", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/c/c3/Coat_of_arms_of_Tyumen_Oblast.svg"),
+            new RegionLocalEntry("RU-UD", "Удмуртская Республика", "Республика", "Ижевск", "UTC+4", "https://upload.wikimedia.org/wikipedia/commons/c/c1/Coat_of_arms_of_Udmurtia.svg"),
+            new RegionLocalEntry("RU-ULY", "Ульяновская область", "Область", "Ульяновск", "UTC+4", "https://upload.wikimedia.org/wikipedia/commons/f/f0/Coat_of_arms_of_Ulyanovsk_Oblast.svg"),
+            new RegionLocalEntry("RU-VGG", "Волгоградская область", "Область", "Волгоград", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/9/9d/Coat_of_arms_of_Volgograd_Oblast.svg"),
+            new RegionLocalEntry("RU-VLA", "Владимирская область", "Область", "Владимир", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/6/64/Coat_of_arms_of_Vladimir_Oblast.svg"),
+            new RegionLocalEntry("RU-VLG", "Вологодская область", "Область", "Вологда", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/7/7a/Coat_of_arms_of_Vologda_Oblast.svg"),
+            new RegionLocalEntry("RU-VOR", "Воронежская область", "Область", "Воронеж", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/7/7e/Coat_of_arms_of_Voronezh_Oblast.svg"),
+            new RegionLocalEntry("RU-YAN", "Ямало-Ненецкий автономный округ", "Автономный округ", "Салехард", "UTC+5", "https://upload.wikimedia.org/wikipedia/commons/1/1a/Coat_of_arms_of_Yamalo-Nenets_Autonomous_Okrug.svg"),
+            new RegionLocalEntry("RU-YAR", "Ярославская область", "Область", "Ярославль", "UTC+3", "https://upload.wikimedia.org/wikipedia/commons/7/7b/Coat_of_arms_of_Yaroslavl_Oblast.svg"),
+            new RegionLocalEntry("RU-YEV", "Еврейская автономная область", "Автономная область", "Биробиджан", "UTC+10", "https://upload.wikimedia.org/wikipedia/commons/4/4b/Coat_of_arms_of_Jewish_Autonomous_Oblast.svg"),
+            new RegionLocalEntry("RU-ZAB", "Забайкальский край", "Край", "Чита", "UTC+9", "https://upload.wikimedia.org/wikipedia/commons/c/c4/Coat_of_arms_of_Zabaykalsky_Krai.svg")
+    );
+
+    private void loadRegionsFromLocalCatalog(Country russia, LoadAccumulator acc) {
+        log.info("Загрузка всех регионов России из локального справочника ({} субъектов)...", RU_REGIONS_LOCAL.size());
+
+        for (RegionLocalEntry entry : RU_REGIONS_LOCAL) {
             try {
-                GeoRegionData geoData = geoDataEnrichmentService.enrichRegion(regionName, russia);
-                if (geoData != null) {
-                    geoData.setCountryName("Россия");
-                    saveOrUpdateRegion(geoData, russia, acc);
-                }
+                GeoRegionData geoData = new GeoRegionData();
+                geoData.setRegionRuName(entry.nameRu);
+                geoData.setRegionEngName(null);
+                geoData.setIsoCode(entry.iso);
+                geoData.setRegionType(entry.type);
+                geoData.setCapital(entry.capital);
+                geoData.setTimeZone(entry.timeZone);
+                geoData.setEmblemUrl(entry.emblemUrl);
+                geoData.setCountryName("Россия");
+                saveOrUpdateRegion(geoData, russia, acc);
             } catch (Exception e) {
-                log.warn("Ошибка загрузки региона '{}': {}", regionName, e.getMessage());
-                acc.errors.add(regionName + ": " + e.getMessage());
+                log.warn("Ошибка загрузки региона '{}': {}", entry.nameRu, e.getMessage());
+                acc.errors.add(entry.nameRu + ": " + e.getMessage());
             }
         }
     }
