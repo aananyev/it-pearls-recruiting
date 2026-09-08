fa7deff8d38b4bc09da2d1c52531f46f2bde7211
Hermes-1 <deneb@yandex.ru>
2026-09-08 11:05:46 +0400

    fix(db): исправление имен колонок monthly_quota_tokens и reason в миграции квоты alan

--- modules/core/db/update/postgres/26/260908-3-setAlanUnlimitedLlmQuota.sql
+++ modules/core/db/update/postgres/26/260908-3-setAlanUnlimitedLlmQuota.sql
@@ -4,7 +4,7 @@
 DELETE FROM hunttech_llm_user_quota_override
 WHERE user_id IN (SELECT id FROM sec_user WHERE login = 'alan' AND delete_ts IS NULL);
 
-INSERT INTO hunttech_llm_user_quota_override (id, version, create_ts, created_by, user_id, quota_tokens, effective_from, note)
+INSERT INTO hunttech_llm_user_quota_override (id, version, create_ts, created_by, user_id, monthly_quota_tokens, effective_from, reason)
 SELECT gen_random_uuid(), 1, current_timestamp, 'admin', u.id, 2147483647, '2026-01-01', 'Безлимитная месячная квота административного API для пользователя alan'
 FROM sec_user u
 WHERE u.login = 'alan' AND u.delete_ts IS NULL
