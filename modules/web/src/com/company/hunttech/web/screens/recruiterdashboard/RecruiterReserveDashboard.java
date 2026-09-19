ae09eac27694f2c797e4142b12d41ba286cf08a5
aananyev <deneb@yandex.ru>
2026-09-19 16:17:59 +0400

    Добавить дашборды рекрутера: RecruiterReserveDashboard.java

---
+++ modules/web/src/com/company/hunttech/web/screens/recruiterdashboard/RecruiterReserveDashboard.java
@@ -0,0 +1,10 @@
+package com.company.hunttech.web.screens.recruiterdashboard;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("hunttech_RecruiterReserveDashboard")
+@UiDescriptor("recruiter-reserve-dashboard.xml")
+public class RecruiterReserveDashboard extends Screen {
+}
